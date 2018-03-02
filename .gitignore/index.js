6b11e45951835c3ec4fdecdbd699d2953da2e486
loniix <36993102+loniix@users.noreply.github.com>
2018-03-02 22:33:11 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,55 +8,55 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'Bonjour') {
-    msg.send('Salutations.');
+    msg.reply('Salutations.');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Comment allez-vous ?') {
-    msg.send('Parfait. Et vous ?');
+    msg.reply('Parfait. Et vous ?');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Ca va ?') {
-    msg.send('En pleine forme ! Et vous ?');
+    msg.reply('En pleine forme ! Et vous ?');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Hein, Screwie ?') {
-    msg.send('Tout à fait, professeur.');
+    msg.reply('Tout à fait, professeur.');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Bonsoir') {
-    msg.send('Bonjour ! Marrant, pas vrai ?');
+    msg.reply('Bonjour ! Marrant, pas vrai ?');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Code C-137 Cornichon') {
-    msg.send('Exécution : Cody est un con. Oops, je me suis trompé.');
+    msg.reply('Exécution : Cody est un con. Oops, je me suis trompé.');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Hey') {
-    msg.send('Bonjour ! Comment vous-sentez-vous ?');
+    msg.reply('Bonjour ! Comment vous-sentez-vous ?');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Le corbeau est brûlé.') {
-    msg.send('Exécution : Screwie > Detenaï.');
+    msg.reply('Exécution : Screwie > Chiss Lambda.');
   }
 });
 
 client.on('message', msg => {
   if (msg.content === 'Pancake du matin.') {
-    msg.send('Intialisation : Bonjour, Professeur.');
+    msg.reply('Intialisation : Bonjour, Professeur.');
   }
 });
 
@@ -65,7 +65,7 @@ client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'bienvenue');
   if (!channel) return;
   console.log('Trouvé 2 !');
-  channel.send(`Bienvenue sur **The Old Republic RP**, ${member}. Je suis **Screwie**, droïde astromech du **créateur** et je vous accompagnerais partout ! Vous devez commencer par lire tous les salons de la catégorie **Informations** avant de vous lancer !`);
+  channel.reply(`Bienvenue sur **The Old Republic RP**, ${member}. Je suis **Screwie**, droïde astromech du **créateur** et je vous accompagnerais partout ! Vous devez commencer par lire tous les salons de la catégorie **Informations** avant de vous lancer !`);
 });
 
 client.login("NDE5MTIxNjYwMTM4ODgxMDI0.DXrnDA.IipiPeIIY8pBs47ypk53WMoqPxk");
